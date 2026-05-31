37d33c9e8b344f4852766bc932b69eedcf9ab52d
Bruce <bruceliu20250113@gmail.com>
2026-05-31 15:29:08 +0800

    Day 4：添加calculator包， 练习模块与包的导入

---
+++ main.py
@@ -0,0 +1,4 @@
+from calculator import add, multiply
+
+print(f"199 与 188 的和为：{add(199,188)}")
+print(f"199 与 188 的乘积为：{multiply(199,188)}")
---
+++ mymath.py
@@ -0,0 +1,5 @@
+def he(a, b):
+    return a + b
+
+def cheng(a, b):
+    return a * b
