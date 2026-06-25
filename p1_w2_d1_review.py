41ac522455ee8dc5938cd2131ff6d74f0cda067a
Bruce <bruceliu20250113@gmail.com>
2026-06-25 11:06:14 +0800

    BankAccount 添加 transfer 方法,withdraw/transfer 改用布尔返回值

--- p1_w2_d1_review.py
+++ p1_w2_d1_review.py
@@ -9,8 +9,26 @@ class BankAccount(object):
     def withdraw(self, amount):
         if self.balance >= amount:
             self.balance -= amount
+            return True
         else:
-            return "Balance is not enough"
+            # print("Balance is not enough")
+            return False
     
     def show_balance(self):
         return f"{self.owner}'s account balance is {self.balance}."
+
+    def transfer(self, other, amount):
+        if self.withdraw(amount):
+            other.deposit(amount)
+            print("Transfer Success")
+            return True
+        else:
+            print('Balance is not enough')
+            return False
+            
+
+a = BankAccount('张三', 100)   # 注意,这次张三只有 100
+b = BankAccount('李四', 500)
+a.transfer(b, 300)             # 张三想转 300,但他只有 100
+print(a.show_balance())
+print(b.show_balance())
