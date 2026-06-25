3d28d585001511361cf347ca309df4eea5d573c1
Bruce <bruceliu20250113@gmail.com>
2026-06-25 22:02:27 +0800

    BankAccount 添加Access Control

---
+++ p1_w2_d2_accon.py
@@ -0,0 +1,34 @@
+class BankAccount(object):
+    def __init__(self, owner, balance = 0):
+        self.__owner = owner
+        self.__balance = balance
+    
+    def deposit(self, amount):
+        self.__balance += amount
+    
+    def withdraw(self, amount):
+        if self.__balance >= amount:
+            self.__balance -= amount
+            return True
+        else:
+            # print("Balance is not enough")
+            return False
+    
+    def show_balance(self):
+        return f"{self.__owner}'s account balance is {self.__balance}."
+
+    def transfer(self, other, amount):
+        if self.withdraw(amount):
+            other.deposit(amount)
+            print("Transfer Success")
+            return True
+        else:
+            print('Balance is not enough')
+            return False
+            
+
+a = BankAccount('张三', 100)   
+a.__balance = 99999999         
+print(a.show_balance())
+print(a.__balance)                    # 99999999 —— 你新建的垃圾属性
+print(a._BankAccount__balance)        # 100 —— 真正的余额,没被动
